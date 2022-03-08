ff79face4c83b8dbb6bdaca3508622afea2c69b4
mv <mv@outsi.de>
2022-03-08 16:19:53 +0300

    add products list

--- Frontend/wmwstudio/apps/admin/src/app/app.module.ts
+++ Frontend/wmwstudio/apps/admin/src/app/app.module.ts
@@ -1,23 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+import { CategoriesListComponent } from './pages/categories/categories-list/categories-list.component';
+import { CategoriesService } from '@wmwstudio/products';
+import { CategoriesFormComponent } from './pages/categories/categories-form/categories-form.component';
+import { ProductsListComponent } from './pages/products/products-list/products-list.component';
+import { ProductsFormComponent } from './pages/products/products-form/products-form.component';
 
-import { CardModule } from 'primeng/card'
-import { ToolbarModule } from 'primeng/toolbar'
-import { ButtonModule } from 'primeng/button'
-import { TableModule } from 'primeng/table'
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog'
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { DropdownModule } from 'primeng/dropdown';
+import { EditorModule } from 'primeng/editor';
 
 const UX_MODULE = [
     CardModule,
     ToolbarModule,
     ButtonModule,
-    TableModule
+    TableModule,
+    InputTextModule,
+    ToastModule,
+    ConfirmDialogModule,
+    ColorPickerModule,
+    InputNumberModule,
+    InputTextareaModule,
+    InputSwitchModule,
+    DropdownModule,
+    EditorModule
 ]
 
 const routes: Routes = [
@@ -32,19 +58,43 @@ const routes: Routes = [
             {
                 path: 'categories',
                 component: CategoriesListComponent
+            },
+            {
+                path: 'categories/form',
+                component: CategoriesFormComponent
+            },
+            {
+                path: 'categories/form/:id',
+                component: CategoriesFormComponent
+            },
+            {
+                path: 'products',
+                component: ProductsListComponent
+            },
+            {
+                path: 'products/form',
+                component: ProductsFormComponent
+            },
+            {
+                path: 'products/form/:id',
+                component: ProductsFormComponent
             }
         ]
     }
 ]
 
 @NgModule({
-    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent],
+    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent, CategoriesFormComponent, ProductsListComponent, ProductsFormComponent],
     imports: [
-        BrowserModule, 
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
         RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
         ...UX_MODULE
     ],
-    providers: [],
+    providers: [CategoriesService, MessageService, ConfirmationService],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ Frontend/wmwstudio/apps/admin/src/app/pages/categories/categories-form/categories-form.component.ts
@@ -0,0 +1,105 @@
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { CategoriesService, Category } from '@wmwstudio/products';
+import { MessageService } from 'primeng/api';
+import { timer } from 'rxjs';
+
+@Component({
+  selector: 'admin-categories-form',
+  templateUrl: './categories-form.component.html',
+  styles: []
+})
+export class CategoriesFormComponent implements OnInit {
+
+  form!: FormGroup
+  isSubmitted: boolean = false
+  editmode: boolean = false
+  currentCategoryId: string = ''
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private categoriesService: CategoriesService,
+    private messageService: MessageService,
+    private location: Location,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      icon: ['', Validators.required],
+      color: ['#ffffff']
+    })
+
+    this._checkEditMode()
+  }
+
+  onSubmit() {
+    this.isSubmitted = true
+    if (this.form.invalid) {
+      return;
+    }
+    const category : Category = {
+      id: this.currentCategoryId,
+      name: this.categoryForm.name.value,
+      icon: this.categoryForm.icon.value,
+      color: this.categoryForm.color.value
+    }
+    if (this.editmode) {
+     this._updateCategory(category)
+    } else {
+      this._addCategory(category)
+    }
+  }
+
+  onBack() {
+    this.location.back()
+  }
+
+  private _checkEditMode() {
+    this.route.params.subscribe(params => {
+      if (params.id) {
+        this.editmode = true
+        this.currentCategoryId = params.id
+        this.categoriesService.getCategory(params.id).subscribe(category => {
+          this.categoryForm.name.setValue(category.name)
+          this.categoryForm.icon.setValue(category.icon)
+          this.categoryForm.color.setValue(category.color)
+        })
+      }
+    })
+  }
+
+  get categoryForm() {
+    return this.form.controls
+  }
+
+  private _updateCategory(category: Category) {
+    this.categoriesService.updateCategory(category).subscribe(
+      (category: Category) => {
+        this.messageService.add({severity: 'success', summary: 'Success', detail: `Category ${category.name} is updated!`})
+        timer(1000).toPromise().then(done => {
+          this.location.back()
+        })
+      }, (error) => {
+        this.messageService.add({severity: 'error', summary: 'Error', detail: 'Category is not updated!'})
+      }
+    )
+  }
+
+  private _addCategory(category: Category) {
+    this.categoriesService.createCategory(category).subscribe(
+      (category: Category) => {
+        this.messageService.add({severity: 'success', summary: 'Success', detail: `Category ${category.name} is created!`})
+        timer(1000).toPromise().then(done => {
+          this.location.back()
+        })
+      }, (error) => {
+        this.messageService.add({severity: 'error', summary: 'Error', detail: `Category is not created!`})
+      }
+    )
+  }
+
+}
